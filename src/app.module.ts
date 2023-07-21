e462c72b54590bd331b92b9cbeb09c7fb671233d
maksim-luzin <333maximax333@gmail.com>
2023-07-21 23:01:15 +0300

    Add config and mongoose module

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ProtectedModule } from './protected/protected.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Config } from './enums/config';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    AuthModule,
+    ProtectedModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.${process.env[Config.Mode]}.env`,
+    }),
+    MongooseModule.forRoot(process.env[Config.DbUrl]),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/enums/config.ts
@@ -0,0 +1,8 @@
+export enum Config {
+  AppPort = 'Port',
+  DbUrl = 'DB_URL',
+  Secret = 'SECRET',
+  Mode = 'NODE_ENV',
+  UserName = 'NAME',
+  UserPassword = 'PASSWORD',
+}
